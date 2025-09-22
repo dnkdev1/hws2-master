51c1acc4ec87860ada1ec21803947f751e14dddf
dnkdev1 <dnkdev@outlook.com>
2025-09-22 23:11:47 +0700

    HW5: sidebar can show and hide

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -44,7 +44,6 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
                         className={preJuniorClass}
                     >
                         Pre-junior
@@ -53,7 +52,6 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
                         className={juniorClass}
                     >
                         Junior
@@ -62,7 +60,6 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        // className={...} // делает студент
                         className={juniorPlusClass}
                     >
                         Junior Plus
