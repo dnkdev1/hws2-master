deb68529ac9c5fa328b33d7ba1110435eb6a0499
dnkdev1 <dnkdev@outlook.com>
2025-10-16 16:01:46 +0700

    HW9 buttons can be enable/disable

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -69,7 +69,13 @@ function Clock() {
     }
 
     const stringTime = (currentTime.format(date)) || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = (currentDate.format(date)) || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    //const stringDate = (currentDate.format(date)) || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+
+
+    const stringDate = `${date.getDate().toString().padStart(2, '0')}.${(date.getMonth() + 1)
+            .toString()
+            .padStart(2, '0')}.${date.getFullYear()}`
+        || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     const stringDay = (currentDayOfWeek.format(date)) || <br/> // пишут студенты
