4b931d16a05f71a54f41ed5fedc8c148c50ba1fc
dnkdev1 <dnkdev@outlook.com>
2025-11-05 13:16:13 +0700

    HW11 is done

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -8,7 +8,9 @@ const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
             sx={{ // стили для слайдера // пишет студент
-                
+                width: '147px',
+                height: '4px',
+                color: 'green'
             }}
             value={props.value}
             defaultValue={props.defaultValue}
